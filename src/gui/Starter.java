7629087896fe443bc5afef3870b019a16f2346cd
Lars Gustafson <lars.gustafson@gmail.com>
2017-11-20 23:54:02 +0100

    Modifed file structure

--- src/gui/Starter.java
+++ src/gui/Starter.java
@@ -1,11 +1,10 @@
 package gui;
 
-import gui.controll.Controller;
-import gui.viewer.Viewer;
-
 import java.io.File;
 import java.io.IOException;
 
+import gui.controll.Controller;
+import gui.viewer.Viewer;
 import model.TalmanModel;
 import settings.Settings;
 
@@ -14,25 +13,17 @@ public class Starter {
 	public static final File DATA_FOLDER = new File("data");
 
 	public static void main(String[] args) throws IOException {
-
 		File folder = DATA_FOLDER;
 		if (args.length > 0)
 			folder = new File(args[0]);
 
 		if (!folder.exists()) {
 			if (!folder.mkdir() || !folder.isDirectory()) {
-				System.err.println("Error creating data folder ("
-						+ folder.getAbsolutePath() + ").");
+				System.err.println("Error creating data folder (" + folder.getAbsolutePath() + ").");
 			}
 		}
 
-		String file = "htm1.settings";
-
-		if (args.length > 0)
-			file = args[0];
-
-		Settings settings = new Settings(new File(file));
-
+		Settings settings = new Settings(folder);
 		TalmanModel model = new TalmanModel(settings);
 		Controller c = new Controller(model);
 		Viewer v = new Viewer(model);
--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -9,28 +9,30 @@ import standalone.csv.CSV;
 
 public class Settings {
 	private CSV csv;
-	private HashMap<String,ArrayList<String>> data; 
-	private static final String[][] PRESETS = new String[][] { 	{"title","Möte"},
-																{"paragraph","§"},
-																{"agenda","data/agenda"}}; 
-	
-	
-	public Settings(File f) throws IOException {
+	private HashMap<String, ArrayList<String>> data;
+	private static final String[][] PRESETS = new String[][] { { "title", "Möte" }, { "paragraph", "§" },
+			{ "agenda", "data/agenda" } };
+
+	public Settings(File folder) throws IOException {
+		if (!folder.isDirectory()) {
+			throw new RuntimeException("Selected folder is not a folder.");
+		}
+		File f = new File(folder, "settings");
+		System.out.printf("Settings file: %s%n", folder);
 		csv = new CSV(f);
 		data = new HashMap<>();
-		
+
 		for (ArrayList<String> row : csv.getData()) {
 			if (row.size() < 2)
 				throw new IOException("Illegal row information");
 			data.put(row.get(0), row);
 		}
-		
+
 		enterPresets();
-		
+
 		csv.save();
 	}
-	
-	
+
 	private void enterPresets() {
 		for (String[] pre : PRESETS) {
 			String key = pre[0];
@@ -41,41 +43,40 @@ public class Settings {
 			if (data.get(key) == null) {
 				data.put(key, row);
 				csv.getData().add(row);
-				System.err.println("Found no entry for " + key + ". Entered standard \""+value+"\"");
-				
+				System.err.println("Found no entry for " + key + ". Entered standard \"" + value + "\"");
+
 			}
 		}
 	}
 
-
 	public synchronized String getTitle() {
 		return get("title");
 	}
-	
+
 	public synchronized void updateTitle(String value) {
-		update("title",value);
+		update("title", value);
 	}
-	
+
 	public synchronized String getParagraph() {
 		return get("paragraph");
 	}
-	
+
 	public synchronized String getAgenda() {
 		return get("agenda");
 	}
-	
+
 	private String get(String title) {
 		return data.get(title).get(1);
 	}
-	
+
 	private void update(String title, String value) {
 		data.get(title).remove(1);
-		data.get(title).add(1,value);
+		data.get(title).add(1, value);
 		try {
 			csv.save();
 		} catch (IOException e) {
 			System.err.println("Changes not saved to file");
 		}
 	}
-	
+
 }
